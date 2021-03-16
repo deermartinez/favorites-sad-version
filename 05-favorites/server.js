ecf3aa899505824e95d30e2eb7865eb475e3305c
deermartinez <deermartinez8@gmail.com>
2021-03-16 10:50:32 -0600

    push

--- 05-favorites/server.js
+++ 05-favorites/server.js
@@ -1,9 +1,9 @@
 // Require dependencies
 //we always need out http
-var http = require("http");
+var http = require('http');
 
 //import the filesystem module
-const fs = require("fs");
+var fs = require('fs');
 //The fs.appendFile() method is used to asynchronously append the given data to a file.
 // A new file is created if it does not exist. The options parameter can be used to modify 
 //the behavior of the operation.
@@ -15,35 +15,46 @@ var PORT = 8080;
 // Capture the url the request is made to
 var server=http.createServer(handleRequest);
 
-// Pass the handleRequest function to empower it with functionality.
-function handleRequest(req,res){
-    var path=req.url;
-}
 
 // When we visit different urls, read and respond with different files
 //this is the switch 
-switch(path){
-    case"/":
-        return displayRoot(res);
+// Pass the handleRequest function to empower it with functionality.
+function handleRequest(req,res){
+    var path=req.url;
+    //url request is made for/to
+    switch(path){
+        case"/":
+            // return displayRoot(res);
+            return renderHTML('index.html',res);
+    
+        case"/movies":
+            
+        case"/food":
+         
+        case"/frameworks":
+        // case"/portfolio":
+        //     return displayPortfolio(res);
+                return renderHTML(path + ".html", res);
+    
+        default:
+            // return index.html(path,res);
+            return display 404(path,res);
+    }
+    // default to rendering index.html, if none of above cases are hit
+    
+};
 
-    case"/portfolio":
-        return displayPortfolio(res);
-
-    default:
-        return index.html(path,res);
+function renderHTML(filePath,res){
+    return fs.readFile(__dirname + filePath, function(err,data){
+        if (err) throw err;
+        //file system data=if error, throw error
+        // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+   // an html file.
+        res.writeHead(200,{"Content-Type":"text/html"});
+        res.end(data);
+    });
 }
-// default to rendering index.html, if none of above cases are hit
-
-
-// Start our server so that it can begin listening to client requests.
-Server.listen(PORT, function () {
-    console.log("Server is listening on PORT: " + PORT)
-});
-  // Log (server-side) when our server has started
-  
-
-
-
+ 
 // * Create a website with four routes:
 //   * Home
 //   * Favorite Foods
@@ -57,3 +68,13 @@ Server.listen(PORT, function () {
 
 // * Have your home page have links to all of your other pages.
 // * DRY up your code by only having a single `readFile`
+
+
+
+
+// Start our server so that it can begin listening to client requests.
+server.listen(PORT, function () {
+    console.log("Server is listening on PORT: " + PORT);
+});
+  // Log (server-side) whetask managern our server has started
+ 
